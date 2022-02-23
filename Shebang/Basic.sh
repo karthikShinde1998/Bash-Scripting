9d3dcaddfabed155ab8ded4bf83770f0e98a6bdf
Karthik Shinde <karthikraoshinde@gmail.com>
2022-02-23 21:45:58 +0530

    Chapter 1

---
+++ Shebang/Basic.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "  Welcome to Shell Scripting \n This is the First Code Ever pushed to GITHUB!\n\n"
+
+echo "Today is \t\t$(date)\n"
+
+
+cal
+
+echo "\n\n These  are the Files in our current  Directory : (i.e $(pwd))\n"
+ls -l
