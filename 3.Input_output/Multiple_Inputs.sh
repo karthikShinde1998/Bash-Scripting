0ee975e2fe90c03e35c9bca0e2667ca7dc2aa723
Karthik Shinde <karthikraoshinde@gmail.com>
2022-02-26 14:20:29 +0530

    LastFile

---
+++ 3.Input_output/Multiple_Inputs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please Enter Your Full Name:"
+read Fname MName Last_Name
+
+echo  -e "\n The Details of the user is as follows:\n First Name : $Fname \n Middle Name : $MName \n Last Name : $Last_Name\n"
+
+# With this approach the unwanted Whitespaces in-between the data entered is truncated/ removed
+
